4994311005a1cac93a2605bf2c83181bbc122f75
Ismail Fahmy <ismailmn05@gmail.com>
2023-05-25 23:57:25 +0300

    qtile config edit

--- qtile/config.py
+++ qtile/config.py
@@ -100,7 +100,7 @@ layouts = [
 ]
 
 widget_defaults = dict(
-    font="sans",
+    font="Caskaydia Cove Nerd Font",
     fontsize=12,
     padding=3,
 )
@@ -108,29 +108,19 @@ extension_defaults = widget_defaults.copy()
 
 screens = [
     Screen(
-        bottom=bar.Bar(
+        top=bar.Bar(
             [
                 widget.CurrentLayout(),
                 widget.GroupBox(),
                 widget.Prompt(),
                 widget.WindowName(),
-                widget.Chord(
-                    chords_colors={
-                        "launch": ("#ff0000", "#ffffff"),
-                    },
-                    name_transform=lambda name: name.upper(),
-                ),
-                widget.TextBox("default config", name="default"),
-                widget.TextBox("Press &lt;M-r&gt; to spawn", foreground="#d75f5f"),
-                # NB Systray is incompatible with Wayland, consider using StatusNotifier instead
-                # widget.StatusNotifier(),
+                # widget.TextBox("default config", name="default"),
                 widget.Systray(),
-                widget.Clock(format="%Y-%m-%d %a %I:%M %p"),
-                widget.QuickExit(),
+                widget.Clock(format="󰃭 %d/%m/%Y   %H:%M"),
             ],
             24,
-            # border_width=[2, 0, 2, 0],  # Draw top and bottom borders
-            # border_color=["ff00ff", "000000", "ff00ff", "000000"]  # Borders are magenta
+            border_width=[2, 0, 2, 0],  # Draw top and bottom borders
+            border_color=["000000", "000000", "ff00ff", "000000"]  # Borders are magenta
         ),
     ),
 ]
