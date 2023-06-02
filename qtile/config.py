dead900f7fbb3ef6af43de1a36c3d2dacfd01562
Ismail Fahmy <ismailmn05@gmail.com>
2023-06-03 01:48:53 +0300

    install guide edits, add wifi and battery widgets to qtile

--- qtile/config.py
+++ qtile/config.py
@@ -12,61 +12,73 @@ mod = "mod4"
 browser = "firefox"
 terminal = guess_terminal()
 keys = [
-    # A list of available commands that can be bound to keys can be found
-    # at https://docs.qtile.org/en/latest/manual/config/lazy.html
+        # A list of available commands that can be bound to keys can be found
+        # at https://docs.qtile.org/en/latest/manual/config/lazy.html
 
-    # Open Browser
-    Key([mod], "b", lazy.spawn(browser), desc="Open set browser"),
-    Key([mod, "shift"], "s",lazy.spawn(f"kitty ls"  )),
-
-
+        # Open Browser
+        Key([mod], "b", lazy.spawn(browser), desc="Open set browser"),
+        Key([mod, "shift"], "s",lazy.spawn(f"kitty ls"  )),
 
 
-    # Switch between windows
-    Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
-    Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
-    Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
-    Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
 
-    # Move windows between left/right columns or move up/down in current stack.
-    # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
-    Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
 
-    # Grow windows. If current window is on the edge of screen and direction
-    # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod, "control"], "left", lazy.layout.grow_left(), desc="Grow window to the left"),
-    Key([mod, "control"], "right", lazy.layout.grow_right(), desc="Grow window to the right"),
-    Key([mod, "control"], "down", lazy.layout.grow_down(), desc="Grow window down"),
-    Key([mod, "control"], "up", lazy.layout.grow_up(), desc="Grow window up"),
-    Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+        # Switch between windows
+        Key([mod], "h", lazy.layout.left(), desc="Move focus to left"),
+        Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
+        Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
+        Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
+        Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
 
-    # Toggle between split and unsplit sides of stack.
-    # Split = all windows displayed
-    # Unsplit = 1 window displayed, like Max layout, but still with
-    # multiple stack panes
-    Key(
-        [mod, "shift"],
-        "Return",
-        lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack",
-    ),
-    Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
-    # Toggle between different layouts as defined below
-    Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
-    Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
-    Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
-    Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
-    Key([mod], "r", lazy.spawn("rofi -show run")),
-    Key([mod, "shift"], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
-    Key(["control", "mod1"], "delete", lazy.spawn(powermenu)),
+        # Move windows between left/right columns or move up/down in current stack.
+        # Moving out of range in Columns layout will create new column.
+        Key([mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"),
+        Key([mod, "shift"], "l", lazy.layout.shuffle_right(), desc="Move window to the right"),
+        Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
+        Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
+
+        # Grow windows. If current window is on the edge of screen and direction
+        # will be to screen edge - window would shrink.
+        Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+        Key([mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"),
+        Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
+        Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
+        Key([mod, "control"], "left", lazy.layout.grow_left(), desc="Grow window to the left"),
+        Key([mod, "control"], "right", lazy.layout.grow_right(), desc="Grow window to the right"),
+        Key([mod, "control"], "down", lazy.layout.grow_down(), desc="Grow window down"),
+        Key([mod, "control"], "up", lazy.layout.grow_up(), desc="Grow window up"),
+        Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
+
+        # Toggle between split and unsplit sides of stack.
+        # Split = all windows displayed
+        # Unsplit = 1 window displayed, like Max layout, but still with
+        # multiple stack panes
+        Key(
+            [mod, "shift"],
+            "Return",
+            lazy.layout.toggle_split(),
+            desc="Toggle between split and unsplit sides of stack",
+            ),
+        Key([mod], "Return", lazy.spawn(terminal), desc="Launch terminal"),
+        # Toggle between different layouts as defined below
+        Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
+        Key([mod, "shift"], "c", lazy.window.kill(), desc="Kill focused window"),
+        Key([mod, "control"], "r", lazy.reload_config(), desc="Reload the config"),
+        Key([mod, "control"], "q", lazy.shutdown(), desc="Shutdown Qtile"),
+        Key([mod], "r", lazy.spawn("rofi -show run")),
+        Key([mod, "shift"], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
+        Key(["control", "mod1"], "delete", lazy.spawn(powermenu)),
+        # MEDIA KEYS
+        Key([], "XF86AudioLowerVolume", lazy.spawn("amixer sset Master 5%-"), desc="Lower Volume by 5%"),
+
+        Key([], "XF86AudioRaiseVolume", lazy.spawn("amixer sset Master 5%+"), desc="Raise Volume by 5%"),
+
+        Key([], "XF86AudioMute", lazy.spawn("amixer sset Master 1+ toggle"), desc="Mute/Unmute Volume"),
+        Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause"), desc="Play/Pause player"),
+
+        Key([], "XF86AudioNext", lazy.spawn("playerctl next"), desc="Skip to next"),
+
+        Key([], "XF86AudioPrev", lazy.spawn("playerctl previous"), desc="Skip to previous"),
+
 
 ]
 
@@ -178,21 +190,21 @@ screens = [
         Screen(
             top=bar.Bar(
                 [
-                widget.Sep(
+                    widget.Sep(
                         linewidth = 0,
                         padding = 6,
                         ),
-               widget.GroupBox(font="Ubuntu Bold",
-                        fontsize = 10,
-                        margin_y = 1,
-                        margin_x = 0,
-                        padding_y = 5,
-                        padding_x = 5,
-                        borderwidth = 3,
-                        highlight_method = "line",
-                        this_current_screen_border=colors[1],
-                        ),
-                widget.Sep(
+                    widget.GroupBox(font="Ubuntu Bold",
+                                    fontsize = 10,
+                                    margin_y = 1,
+                                    margin_x = 0,
+                                    padding_y = 5,
+                                    padding_x = 5,
+                                    borderwidth = 3,
+                                    highlight_method = "line",
+                                    this_current_screen_border=colors[1],
+                                    ),
+                    widget.Sep(
                         linewidth = 0,
                         padding = 6,
                         ),
@@ -202,6 +214,9 @@ screens = [
                         format="{name}",
                         max_chars=90,
                         ),
+                    widget.Battery(charge_char="󰂄", discharge_char="󰂍", empty_char="󱃍", format="{percent:2.0% {char}}"),
+                    widget.Wlan(disconnected_message="󰖪",format='{essid} {percent:2.0%}'),
+                    widget.ThermalZone(),
                     widget.Volume(
                         fmt="\uf026 {}",
                         mouse_callbacks={
