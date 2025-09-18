ef202916053cf2c0ce5d67428ace25487adc7736
Ismail Fahmy <ismailmn05@gmail.com>
2025-09-18 10:20:59 +0200

    ana msh 3aref 3amalt eih

--- fish/config.fish
+++ fish/config.fish
@@ -67,11 +67,11 @@ abbr    drv     "pushd /run/media/fahmy"
 alias vim='nvim'
 
 # Changing "ls" to "exa"
-alias ls='exa -al --color=always --group-directories-first --icons' # my preferred listing
-alias la='exa -a --color=always --group-directories-first --icons'  # all files and dirs
-alias ll='exa -l --color=always --group-directories-first --icons'  # long format
-alias lt='exa -aT --color=always --group-directories-first --icons' # tree listing
-alias l.='exa -al --color=always --group-directories-first --icons | grep -E "\W\.\w" '
+alias ls='eza -al --color=always --group-directories-first --icons' # my preferred listing
+alias la='eza -a --color=always --group-directories-first --icons'  # all files and dirs
+alias ll='eza -l --color=always --group-directories-first --icons'  # long format
+alias lt='eza -aT --color=always --group-directories-first --icons' # tree listing
+alias l.='eza -al --color=always --group-directories-first --icons | grep -E "\W\.\w" '
 
 #========================================================================================
 
